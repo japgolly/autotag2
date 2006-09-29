2040a00a32c437024a2ca541e40c412e52ec0cbe
Golly <Golly@12e5c2a3-296c-ae4c-98a6-ce9f107eb639>
2006-09-29 14:46:15 +0000

    Refactored config module a little and made constants for all config options
    git-svn-id: file:///tmp/autotag2/svn-repo/trunk/v2@58 12e5c2a3-296c-ae4c-98a6-ce9f107eb639

--- autotag/engine/config.rb
+++ autotag/engine/config.rb
@@ -7,51 +7,49 @@ module Autotag
       
       # Returns an array of regexs to match dir/file names.
       def file_patterns(type)
-        case type
-        when :artist
-          [/^(.+)$/u]
-        when :album
-          [
-            /^(.{4}) - (.+)$/u,
-            /^(.{4})\. (.+)$/u,
-          ]
-        when :cd
-          [/^(?:cd|disc) ([a-z0-9]+)$/iu]
-        when :track
-          [
-            /^(.{2}) - (.+)\.(?:mp3|flac)$/iu,
-            /^(.{2})\. (.+)\.(?:mp3|flac)$/iu,
-          ]
-        else raise
-        end
+        FILE_PATTERNS[type] or raise
       end
       
       # Returns an array of regexs of dir/file names to ignore.
       def file_ignore_patterns(type)
-        [/^\./]
+        FILE_IGNORE_PATTERNS
       end
       
       # Returns an array of metadata attributes that can be ignored when
       # comparing existing tags to new tags (in order to determine whether
       # or not files are up-to-date).
       def ignorable_attributes#(tag)
-        [:_padding,:_tool]
+        IGNORABLE_ATTRIBUTES
       end
       
       # Returns an array of metadata attributes that will be copied from
       # existing tags to new tags.
       def preservable_attributes
-        [:replaygain_track_gain, :replaygain_album_gain, :replaygain_track_peak, :replaygain_album_peak]
+        PRESERVABLE_ATTRIBUTES
       end
       
-      # TODO Add format arg (ie. mp3, flac, etc)
+      # Will eventually add format arg (ie. mp3, flac, etc)
       def tags_to_write(header)
-        if header
-          [ID3v2]
-        else
-          [APEv2]
-        end
+        header ? TAGS_TO_WRITE_HEADER : TAGS_TO_WRITE_FOOTER
       end
+      
+      #--------------------------------------------------------------------------
+      private
+      
+      sep= '(?: +- +|\. *)'
+      FILE_PATTERNS= {
+        :artist => [/^(.+)$/],
+        :album  => [Regexp.new('^(.{4})'+sep+'(.+)$',nil,'U')],
+        :cd     => [/^(?:cd|disc) +([a-z0-9]+)$/iu],
+        :track  => [Regexp.new('^(.{2})'+sep+'(.+)\.(?:mp3|flac)$',true,'U')],
+      }
+      FILE_IGNORE_PATTERNS= [/^\./]
+      IGNORABLE_ATTRIBUTES= [:_padding,:_tool]
+      PRESERVABLE_ATTRIBUTES= [:replaygain_track_gain, :replaygain_album_gain, :replaygain_track_peak, :replaygain_album_peak]
+      TAGS_TO_WRITE_HEADER= [ID3v2]
+      TAGS_TO_WRITE_FOOTER= [APEv2]
+      
+      freeze_all_constants
       
     end # module Config
   end # class Engine
