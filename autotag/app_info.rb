37c84815a7326d785371b19451e4fe1c50d16954
Golly <Golly@12e5c2a3-296c-ae4c-98a6-ce9f107eb639>
2006-10-03 02:52:45 +0000

    Changed app version to 2 [beta]
    git-svn-id: file:///tmp/autotag2/svn-repo/trunk/v2@79 12e5c2a3-296c-ae4c-98a6-ce9f107eb639

--- autotag/app_info.rb
+++ autotag/app_info.rb
@@ -1,6 +1,6 @@
 module Autotag
-  TITLE= "Golly's Autotagger"
-  VERSION= '2_DEV'
+  TITLE= "Golly's AutoTagger"
+  VERSION= '2 [beta]'
   COPYRIGHT= 'Copyright (c) 2006 David Barri. All rights reserved.'
   
   TITLE_AND_VERSION= "#{TITLE} v#{VERSION}"
