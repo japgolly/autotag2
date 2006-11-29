4e30b27d561e63fddc6be7afe7cfd19a7169c678
Golly <Golly@12e5c2a3-296c-ae4c-98a6-ce9f107eb639>
2006-11-29 12:19:40 +0000

    Updated version to RC2
    git-svn-id: file:///tmp/autotag2/svn-repo/trunk/v2@118 12e5c2a3-296c-ae4c-98a6-ce9f107eb639

--- autotag/app_info.rb
+++ autotag/app_info.rb
@@ -1,6 +1,6 @@
 module Autotag
   TITLE= "Golly's AutoTagger"
-  VERSION= '2 RC1'
+  VERSION= '2 RC2'
   COPYRIGHT= 'Copyright (c) 2006 David Barri. All rights reserved.'
   
   TITLE_AND_VERSION= "#{TITLE} v#{VERSION}"
