69f305d154cf518bec1fa2e52b523a97f35b0283
Golly <Golly@12e5c2a3-296c-ae4c-98a6-ce9f107eb639>
2009-03-01 03:46:38 +0000

    Changing version to 2.1.1 WIP
    git-svn-id: file:///tmp/autotag2/svn-repo/trunk/v2@135 12e5c2a3-296c-ae4c-98a6-ce9f107eb639

--- autotag/app_info.rb
+++ autotag/app_info.rb
@@ -1,6 +1,6 @@
 module Autotag
   TITLE= "Golly's AutoTagger"
-  VERSION= '2.1.0'
+  VERSION= '2.1.1 WIP'
   COPYRIGHT= "Copyright (c) 2006-#{Time.now.year} David Barri. All rights reserved."
   
   TITLE_AND_VERSION= "#{TITLE} v#{VERSION}"
