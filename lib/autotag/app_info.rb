0989ff2816839eea9b7430089e39684d0e49a627
David Barri <japgolly@gmail.com>
2012-03-10 15:45:57 +1100

    Added a .version file

--- lib/autotag/app_info.rb
+++ lib/autotag/app_info.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 module Autotag
   TITLE= "Golly's AutoTagger"
-  VERSION= '2.1.2-SNAPSHOT'
+  VERSION= File.read(File.expand_path('../../../.version',__FILE__)).chomp
   COPYRIGHT= "Copyright (c) 2006-#{Time.now.year} David Barri. All rights reserved."
 
   TITLE_AND_VERSION= "#{TITLE} v#{VERSION}"
