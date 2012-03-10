54bc0fdf5854d71285d3f87e04c43dcb04de9b52
David Barri <japgolly@gmail.com>
2012-03-10 13:29:38 +1100

    Fixed misc_test

--- test/misc_test.rb
+++ test/misc_test.rb
@@ -6,23 +6,23 @@ class MiscTest < Autotag::TestCase
   include Autotag::UnicodeIO
 
   def test_unicodeio_glob
-    ac_files= ["code_stats.rb", "autotag", "autotag.rb"]
+    ac_files= ["code_stats.rb", "Gemfile", "Gemfile.lock"]
     ac_files<< "CHANGELOG" if Autotag::Utils::case_insensitive_filenames?
-    assert_equal ac_files.sort, glob(0,nil,"{a,c}*").sort
-    assert_equal ["autotag.rb", "code_stats.rb"], glob(0,nil,'*.rb')
-    assert_equal ["autotag/engine.rb", "test/engine_test.rb"], glob(1,nil,'e*.rb')
-    assert_equal ["autotag/engine.rb"], glob(0,'autotag','e*.rb')
-    assert_equal ["autotag/engine/misc.rb", "test/misc_test.rb"], glob(-1,nil,'mi*.rb')
+    assert_equal ac_files.sort, glob(0,nil,"{c,G}*").sort
+    assert_equal ["code_stats.rb"], glob(0,nil,'*.rb')
+    assert_equal ["test/engine_test.rb"], glob(1,nil,'e*.rb')
+    assert_equal ["lib/autotag/engine.rb"], glob(0,'lib/autotag','e*.rb')
+    assert_equal ["lib/autotag/engine/misc.rb", "test/misc_test.rb"], glob(-1,nil,'mi*.rb')
     assert_equal [], glob(0,nil,'mi*.rb')
     assert_equal ["test/misc_test.rb"], glob(1,nil,'mi*.rb')
-    assert_equal ["autotag/engine/misc.rb", "test/misc_test.rb"], glob(2,nil,'mi*.rb')
+    assert_equal ["lib/autotag/engine/misc.rb", "test/misc_test.rb"], glob(3,nil,'mi*.rb')
   end
 
   def test_unicodeio_file_dir_bools
-    assert file?('autotag.rb')
-    assert !directory?('autotag.rb')
-    assert !file?('autotag')
-    assert directory?('autotag')
+    assert file?('bin/autotag')
+    assert !directory?('bin/autotag')
+    assert !file?('lib/autotag')
+    assert directory?('lib/autotag')
   end
 
 end
